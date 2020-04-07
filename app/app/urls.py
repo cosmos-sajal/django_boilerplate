cb52f34a128701cb18652a3c2a880fa145ec0a5d
Sajal Sharma <sajal.4591@gmail.com>
2020-04-07 17:22:56 +0530

    version user API

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include('user.urls'))
+    path('api/v1/user/', include('user.v1.urls'))
 ]
