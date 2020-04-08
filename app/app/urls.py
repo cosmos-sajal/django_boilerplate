fe95c8c3f8a3f94bc295d48942f7910d8f290fbe
Sajal Sharma <sajal.4591@gmail.com>
2020-04-08 17:45:27 +0530

    add swagger

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
+    url(r'^$', schema_view),
     path('admin/', admin.site.urls),
     path('api/v1/user/', include('user.v1.urls'))
 ]
