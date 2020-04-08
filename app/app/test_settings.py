316d8291ef20d6498bb4694e576e0ed1170fb95b
Sajal Sharma <sajal.4591@gmail.com>
2020-04-08 17:20:03 +0530

    remove core

--- app/app/test_settings.py
+++ app/app/test_settings.py
@@ -41,5 +41,5 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=user,core,helpers',
+    '--cover-package=user,helpers',
 ]
